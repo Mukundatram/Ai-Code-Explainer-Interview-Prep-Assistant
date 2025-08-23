b6629e2d8cd6d88b731ba51cb8bf731b2ccc6010
Mukund Atram <atrammukund42@gmail.com>
2025-08-23 23:18:36 +0530

    updated requirememnts

--- core/hf_llm.py
+++ core/hf_llm.py
@@ -5,15 +5,6 @@ from langsmith import traceable
 from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
-# llm = HuggingFaceEndpoint(
-#     repo_id="deepseek-ai/DeepSeek-R1",
-#     task="text-generation",
-#     temperature=0.1,
-#     max_new_tokens=300,
-#     huggingfacehub_api_token=os.getenv("HF_TOKEN")
-# )
-
-# model = ChatHuggingFace(llm=llm)
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0.7)
 @traceable(name="LLM_Query_for_Assistant")
